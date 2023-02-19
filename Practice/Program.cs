9402b25fd5ef4e7c50314e2109e35747374754cd
rovsencode <rovshanakh@code.edu.az>
2023-02-19 21:54:09 +0400

    C# practice

---
+++ Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿#region Directory and File (Create,Delete,Exists)
+
+//Directory.CreateDirectory(@"C:/Users/HP/Desktop/Test");
+//Directory.CreateDirectory(@"C:/Users/HP/Desktop/Lorem");
+//Directory.CreateDirectory(@"C:/Users/HP/Desktop/Lorem/Tset");
+//Directory.Delete(@"C:/Users/HP/Desktop/Lorem", true);
+//if (Directory.Exists(@"C:/Users/HP/Desktop/Lorem/Tset"))
+//{
+//    Directory.Delete(@"C:/Users/HP/Desktop/Lorem/Tset");
+//}
+//string[] arr = Directory.GetFiles(@"C:/Users/HP/Desktop/Test");
+//foreach (var item in arr)
+//{
+//    Console.WriteLine(item);
+//}
+
+//if (File.Exists(@"C:/Users/HP/Desktop/Test/blabla.txt"))
+//{
+//    //File.Create(@"C:/Users/HP/Desktop/Test/blabla.txt");
+//    File.Delete(@"C:/Users/HP/Desktop/Test/blabla.txt");
+
+//}
+#endregion
